4175bce1c8679dc87ce0dd0ae6354f241688a478
djukic03 <djukic.nemanja003@gmail.com>
2025-06-06 11:47:46 +0200

    extra use import removed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, use } from 'react'
+import { useState, useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
