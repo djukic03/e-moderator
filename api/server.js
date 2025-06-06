ad22959abde9c19768325feb9ff5bc390447d4a4
djukic03 <djukic.nemanja003@gmail.com>
2025-06-06 17:07:46 +0200

    Please deploy

--- api/server.js
+++ api/server.js
@@ -10,7 +10,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ fruits: ["apple", "banana", "cherry"] });
+  res.send({ fruits: ["apple", "banana", "cherry"] });
 });
 
 app.listen(8080, () => {
