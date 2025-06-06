b5501fb44c73bce7d01d2f48f28aa20ab5c909f1
djukic03 <djukic.nemanja003@gmail.com>
2025-06-06 12:25:37 +0200

    vercel error

--- server/server.js
+++ server/server.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-const { createServer } = require("@vercel/node");
-
 const app = express();
+
 const corsOptions = {
   origin: ["https://e-moderator-front.vercel.app"],
   credentials: true,
 };
-
-app.use(cors());
-
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get("/api", (req, res) => {
   res.json({ fruits: ["apple", "banana", "cherry"] });
+});
+
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
 });
 
 module.exports = app;
